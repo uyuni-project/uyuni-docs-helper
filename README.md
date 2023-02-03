144aac67afbd9fe478b28ef0acc5ee112184cc44
Julio Gonzalez Gil <jgonzalez@suse.com>
2023-02-03 14:17:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Repository purpose
+
+To provide a way of building a container image to build the [Uyuni documentation](https://github.com/uyuni-project/uyuni-docs), as it requires to install a big set of tools using `zypper`, `nvm`, `cpan`, `gem` etc.
+
+Besides the container, a helper is provider so people that does not know anything about containers can easily use them.
+
+This a result of [SUSE's Hack Week 22](https://hackweek.opensuse.org/22/projects/testing-gnu-slash-linux-distributions-on-uyuni)
+
+# Repository structure
+
+- `image` directory: Contains the Dockerfile, the `startup.sh` script for the container, and the `build-and-publish` helper (that can be used to build and publish the container after changing whatever is needed on the other two files)
+- `uyuni-doc-helper`: The bash script that allows any contributor to build, inspect the outputs of the build procedure via HTTP and even get outputs if building from a git local clone.
+
+# Requirements
+
+## For everyone
+
+- Have `podman` installed (recommended)
+
+or
+
+- Have `docker` installed and an alias `podman` that calls `docker` (for bash that means adding `alias docker='podman'` to the file `~/.alias`)
+
+
+## For image maintainers
+
+For publishing the image, you will need a [Docker Hub](https://hub.docker.com/), and then talk to [Julio González Gil](https://build.opensuse.org/users/juliogonzalezgil) to get added as maintainer at the Docker Hub.
+
+We plan to move the images to [GitHub packages](https://docs.github.com/en/actions/publishing-packages/publishing-docker-images#publishing-images-to-github-packages) in the future.
+
+
---
+++ image/startup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+
+source ~/.bashrc
+echo "===================================================================="
+if [ ! -d /tmp/uyuni-docs ]; then
+echo " Git repository: ${GITREPO}"
+echo " Git reference: ${GITREF}"
+fi
+echo " Product: ${PRODUCT}"
+echo " Make command: ${COMMAND}"
+echo "===================================================================="
+
+cd ~
+rm -rf uyuni-docs
+
+if [ -d /tmp/uyuni-docs ]; then
+  cp -r /tmp/uyuni-docs ~
+  rm -rf /tmp/uyuni-docs/build
+else 
+  git clone -b ${GITREF} --depth 1 ${GITREPO} uyuni-docs
+fi
+
+cd uyuni-docs
+make configure-${PRODUCT}
+make all-${PRODUCT}
+
+if [ -d /tmp/uyuni-docs ]; then
+  cp -r ~/uyuni-docs/build /tmp/uyuni-docs
+fi
+
+if [ "${SERVE}" == "1" ]; then
+  echo "INFO: Serving the doc on port 8000 on the container"
+  echo "INFO: To access the doc from outside the container, check its mapping"
+  echo "INFO: Most likely you can access the doc via http://localhost:8000"
+  python3 -m http.server
+fi
